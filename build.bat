393b21007f9456c6234114599321e4536b561177
Nir Cohen <nircoh@weizmann.ac.il>
2026-03-05 21:23:45 +0200

    fix: call scripts correctly on windows and expand wildcards

--- build.bat
+++ build.bat
@@ -3,4 +3,6 @@ setlocal enabledelayedexpansion
 
 python -m pip install nanobind --quiet
 python -m build --wheel --outdir "%SRC_DIR%\dist\wheels" --no-isolation --skip-dependency-check
-python -m pip install "%SRC_DIR%\dist\wheels\*.whl" --no-deps --no-build-isolation --prefix "%PREFIX%" -vv
+for %%f in ("%SRC_DIR%\dist\wheels\*.whl") do (
+    python -m pip install "%%f" --no-deps --no-build-isolation --prefix "%PREFIX%" -vv
+)
