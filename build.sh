15cad58f1dce4fbdee48acb6d323b1750af261b4
Nir Cohen <nircoh@weizmann.ac.il>
2026-03-05 21:04:55 +0200

    build

--- build.sh
+++ build.sh
@@ -2,6 +2,4 @@
 set -ex
 
 python -m build --wheel --outdir "$SRC_DIR/dist/wheels" --no-isolation --skip-dependency-check
-mkdir -p "$RECIPE_DIR/dist"
-cp "$SRC_DIR/dist/wheels/"*.whl "$RECIPE_DIR/dist/"
-python -m pip install "$SRC_DIR/dist/wheels/"*.whl --no-deps --no-build-isolation -vv
+python -m pip install "$SRC_DIR/dist/wheels/"*.whl --no-deps --no-build-isolation --prefix "$PREFIX" -vv
